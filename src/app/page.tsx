cdb4184c30367c3b8bc6c6e67429e9fa2dbbfce4
wilypradana <pradanawily28@gmail.com>
2025-04-13 22:59:36 +0700

    added condition in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import HeroSectionOne from "../components/ui/HeroSectionOne";
 import Navbar from "../components/defaultUi/navbar";
 import { Subjects } from "@/components/defaultUi/subjects";
+import { BASE_API_URL } from "../../utils/constant";
 export default function Page() {
+  if(!BASE_API_URL){
+    return null;
+  }
   return (
     <div>
       <Navbar></Navbar>
