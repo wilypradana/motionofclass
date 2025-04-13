0b9b8c6a82c4adcd7f5cebd33a60d0582eda40df
wilypradana <pradanawily28@gmail.com>
2025-04-13 23:22:04 +0700

    add ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://ui.aceternity.com/linear.webp")],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
